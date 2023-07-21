8d10a2d50301aef92b23b37720a53ec0550e089b
BURKHARDT <burkhardt30tto@gmail.com>
2023-07-21 09:57:00 +0500

    Added process and testimonials

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,18 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
+import 'swiper/css/effect-fade';
+
+import 'fslightbox/index.js';
 
 import burgerInit from './modules/burger.js';
+import showMore from './modules/showmore.js';
 
 burgerInit();
+showMore();
 
 mobileSliderInit('.features__slider', {
 	media: 768,
@@ -82,5 +87,56 @@ new Swiper('.cases__slider', {
 	navigation: {
 		nextEl: '.cases__next',
 		prevEl: '.cases__prev',
+	},
+});
+new Swiper('.process__slider', {
+	modules: [Navigation, Pagination, EffectFade],
+	slidesPerView: 1,
+	effect: 'fade',
+	pagination: {
+		el: '.process__pagination',
+		type: 'fraction',
+	},
+	navigation: {
+		nextEl: '.process__next',
+		prevEl: '.process__prev',
+	},
+});
+new Swiper('.testimonials__video', {
+	modules: [Navigation],
+	spaceBetween: 30,
+	slidesPerView: 1.5,
+	centeredSlides: true,
+	navigation: {
+		nextEl: '.testimonials__video-next',
+		prevEl: '.testimonials__video-prev',
+	},
+	breakpoints: {
+		500: {
+			centeredSlides: false,
+			slidesPerView: 2.5,
+			spaceBetween: 20,
+		},
+		768: {
+			centeredSlides: false,
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		992: {
+			centeredSlides: false,
+			slidesPerView: 4,
+			spaceBetween: 20,
+		},
+	}
+});
+new Swiper('.testimonials__messages', {
+	modules: [Navigation],
+	spaceBetween: 60,
+	slidesPerView: 1,
+	autoHeight: true,
+	loop: true,
+	navigation: {
+		nextEl: '.testimonials__messages-next',
+		prevEl: '.testimonials__messages-prev',
 	},
 });
